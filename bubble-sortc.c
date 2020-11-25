63fd400e5ec9c0ce5535b15dccf12637c480d298
Gabriel <gabrielgmusskopf@gmail.com>
2020-11-24 22:47:30 -0300

    implementação do algoritmo de bubble sorting

--- bubble-sortc.c
+++ bubble-sortc.c
@@ -1,3 +1,16 @@
+/**
+ *  Bubble Sort
+ *  Percorre várias vezes o vetor, jogando o maior valor para o final da lista
+ *
+ *  ex: [3,1,5,2]
+ *      [1,3,5,2]
+ *      [1,3,2,5]
+ *      [1,2,4,5]
+ *
+ *  Pior caso   O(n^2)
+ *  Melhor caso O(n)
+ */
+
 #include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -5,6 +18,11 @@
 #include <time.h>
 
 
+/**
+ * Exibe os elementos do array
+ * @param arr array para ser exibido
+ * @param size tamanho do array
+ */
 void display(int* arr, int size){
 
     for(int i=0; i< size; i++){
@@ -14,6 +32,11 @@ void display(int* arr, int size){
 }
 
 
+/**
+ * Implementação do algoritmo bubble sort
+ * @param arr array para ser ordenado
+ * @param size tamanho do array
+ */
 void bubbleSort(int* arr, int size){
 
     for(int i=0; i<size-1; i++){
@@ -33,6 +56,11 @@ void bubbleSort(int* arr, int size){
 }
 
 
+/**
+ * Realiza a troca de elementos
+ * @param primeiro ponteiro para o primeiro elemento
+ * @param segundo ponteiro para o segundo elemento
+ */
 void swap(int* primeiro, int* segundo){
     int aux = *primeiro;
     *primeiro = *segundo;
@@ -46,13 +74,15 @@ int main(void){
     srand(time(NULL));
     int n = rand()%20;
 
+
+    /* Criando um array dinâmico, com n elementos, todos zerados por causa do calloc */
     int *arr = (int*) calloc(n, sizeof(int));
 
     if(arr == NULL){
         printf("Memoria nao alocada!");
         exit(0);
     }
-
+    /* Criando array aleatório de tamanho n */
     for(int i=0; i<n ; i++){
         arr[i] = rand()%100;
     }
